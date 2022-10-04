e2ad833904a4d1705744b678b851f47e222dd979
Abha-saxena123 <85979768+Abha-saxena123@users.noreply.github.com>
2022-10-04 21:43:01 +0530

    Update handle-form.ts

--- pages/api/handle-form.ts
+++ pages/api/handle-form.ts
@@ -1,53 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import exceljs from "exceljs";
+import { connectToDatabase } from "../../lib/mongodb";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   try {
-    let nameFileExcel = `pages/api/test.xlsx`;
-    const workbook = new exceljs.Workbook();
-    const day = new Date(req.body["Date"]).toLocaleString("default", {
-      day: "2-digit",
-    });
-    const month = new Date(req.body["Date"]).toLocaleString("default", {
-      month: "long",
-    });
-    //column = Category	Month	Day	Type	Amount	Remarks
-    const headers = [
-      "Category",
-      "Type",
-      "Date",
-      "Day",
-      "Month",
-      "Amount",
-      "Remarks",
-    ];
-    const column = ["A", "B", "C", "D", "E", "F", "G"];
-    workbook.xlsx.readFile(nameFileExcel).then(function () {
-      const worksheet = workbook.getWorksheet(1);
-      const lastRow = worksheet.lastRow ? worksheet.lastRow.number + 1 : 2;
-      const getRowInsert = worksheet.getRow(lastRow);
-      column.map((c, i) => {
-        getRowInsert.getCell(c).value = req.body[headers[i]];
-      });
+    let { db } = await connectToDatabase();
 
-      getRowInsert.getCell("D").value = day;
-      getRowInsert.getCell("E").value = month;
-
-      getRowInsert.commit();
-      return workbook.xlsx.writeFile(nameFileExcel);
+    const newItem = req.body;
+    db.collection("expense").insertOne(newItem, function (err: any, res: any) {
+      if (err) {
+        console.log("ERROR", err);
+        throw err;
+      }
+      console.log("1 Item inserted");
     });
-    res.redirect("/?status=success");
     res.status(200);
+    res.redirect("/?status=success");
   } catch (e) {
-    res.redirect("/?status=failure");
     res.status(500);
+    res.redirect("/?status=failure");
   }
 }
