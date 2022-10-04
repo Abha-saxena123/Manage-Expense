a387b27b659bf67cbbde8407763febe44a6b8f21
Abha-saxena123 <85979768+Abha-saxena123@users.noreply.github.com>
2022-10-04 22:12:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://abha-saxena123.github.io/Manage-Expense/:path*',
+          },
+        ]
+      },
+
 }
