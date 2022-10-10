fffb5dcf525218776b408afe9a5f96c0fa5d7057
Abha-saxena123 <85979768+Abha-saxena123@users.noreply.github.com>
2022-10-10 20:33:25 +0530

    Update handle-form.ts
    debugging

--- pages/api/handle-form.ts
+++ pages/api/handle-form.ts
@@ -9,10 +9,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@");
   try {
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@=============================");
     let { db } = await connectToDatabase();
 
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@1111111111111111111111");
+
     const newItem = req.body;
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@22222222222222222222222222222");
     db.collection("expense").insertOne(newItem, function (err: any, res: any) {
       if (err) {
         console.log("ERROR", err);
@@ -20,6 +25,7 @@ export default async function handler(
       }
       console.log("1 Item inserted");
     });
+    console.log("#########################22222222222222222222222222222");
     res.status(200).send({ message: "Item inserted successfully" });
     // res.redirect("/?status=success");
   } catch (e) {
